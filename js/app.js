db073db10de9c0cb9034bfd7ee50c2d9e3273d1f
Arthur Diogenes <arthurdfleite@gmail.com>
2024-04-26 16:00:16 -0300

    Implementando a função limpar

--- js/app.js
+++ js/app.js
@@ -1,20 +1,38 @@
+let valorTotal = 0;
+document.getElementById('lista-produtos').innerHTML = 'Nenhum';
+document.getElementById('valor-total').innerHTML = 'R$ 0';
+
 function adicionar() {
+
+    //recuperar valores do produto
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
 
+    //calcular preço
     let preco = quantidade * valorUnitario;
-    let carrinho = document.getElementById('lista-produtos');
+
+    //adicionar ao carrinho
+    let carrinho = document.getElementById('lista-produtos');    
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
     <section class="carrinho__produtos__produto">
       <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
     </section>`;
 
+    //calcular valor total
+    valorTotal += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${valorTotal}`;
+
+    //limpar campo quantidade
+    document.getElementById('quantidade').value = '';
 
 
 }
 
 function limpar() {
-
+    let valorTotal = 0;
+    document.getElementById('lista-produtos').innerHTML = 'Nenhum';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
 }
