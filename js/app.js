259620ce22651b2d7160f3a580a82f150a1c5477
ArthurDiogenes <128557042+ArthurDiogenes@users.noreply.github.com>
2024-04-26 15:36:00 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade');
+
+    let preco = quantidade.value * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML
+
+
+
+}
+
+function limpar() {
+
+}
